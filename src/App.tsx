50c5f277bdab81593e9ec2555952dd5d333d28fa
Dawid Olczak <dawid.a.olczak@gmail.com>
2022-11-27 17:09:10 +0100

    added admin

--- src/App.tsx
+++ src/App.tsx
@@ -2,52 +2,11 @@ import React from 'react';
 import {useAuth} from "./hooks/useAuth";
 import { AuthenticatedApp } from './pages/AuthenticatedApp';
 import { UnAuthenticatedApp } from './pages/UnAuthenticatedApp';
-import {Rightbar} from "./components/Rightbar/Rightbar";
-import {AppBar, Box, Container, CssBaseline, Drawer, Grid, Stack, Toolbar, Typography} from "@mui/material";
-import {Sidebar} from "./components/Sidebar/Sidebar";
-import {HomePage} from "./pages/HomePage";
-import MenuIcon from '@mui/icons-material/Menu';
-import IconButton from "@mui/material/IconButton";
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import {AppBarMobileView} from "./components/AppBarMobileView/AppBarMobileView";
-import {DrawerMobileView} from "./components/DrawerMobileView/DrawerMobileView";
-
-
-const drawerSideBarWidth = 240;
-const drawerRightBarWidth = 130;
 
 export const App = () => {
-    const [mobileOpenSideBar, setMobileOpenSideBar] = React.useState(false);
-    const [mobileOpenRightBar, setMobileOpenRightBar] = React.useState(false);
-
-    const handleDrawerToggleSideBar = () => {
-        setMobileOpenSideBar(!mobileOpenSideBar);
-    };
-
-    const handleDrawerToggleRightBar = () => {
-        setMobileOpenRightBar(!mobileOpenRightBar);
-    };
-
   const { user } = useAuth();
-  // {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
   return (
       <>
-          <Box sx={{ display: 'flex' }}>
-              <CssBaseline />
-              <AppBarMobileView
-                  drawerSideBarWidth={drawerSideBarWidth}
-                  handleDrawerToggleSideBar={handleDrawerToggleSideBar}
-                  handleDrawerToggleRightBar={handleDrawerToggleRightBar}
-              />
-              <DrawerMobileView
-                  drawerSideBarWidth={drawerSideBarWidth}
-                  mobileOpenSideBar={mobileOpenSideBar}
-                  handleDrawerToggleSideBar={handleDrawerToggleSideBar}
-                  drawerRightBarWidth={drawerRightBarWidth}
-                  mobileOpenRightBar={mobileOpenRightBar}
-                  handleDrawerToggleRightBar={handleDrawerToggleRightBar}
-              />
-          </Box>
           {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
       </>
   )
--- src/pages/UnAuthenticatedApp.tsx
+++ src/pages/UnAuthenticatedApp.tsx
@@ -5,19 +5,57 @@ import { HomePage } from './HomePage';
 import {PortfolioPage} from "./PortfolioPage";
 import {AboutMePage} from "./AboutMePage";
 import {EducationPage} from "./EducationPage";
+import {ContactPage} from "./ContactPage";
+import {Box, CssBaseline} from "@mui/material";
+import {AppBarMobileView} from "../components/AppBarMobileView/AppBarMobileView";
+import {DrawerMobileView} from "../components/DrawerMobileView/DrawerMobileView";
+
+const drawerSideBarWidth = 240;
+const drawerRightBarWidth = 130;
 
 export const UnAuthenticatedApp = () => {
+
+    const [mobileOpenSideBar, setMobileOpenSideBar] = React.useState(false);
+    const [mobileOpenRightBar, setMobileOpenRightBar] = React.useState(false);
+
+    const handleDrawerToggleSideBar = () => {
+        setMobileOpenSideBar(!mobileOpenSideBar);
+    };
+
+    const handleDrawerToggleRightBar = () => {
+        setMobileOpenRightBar(!mobileOpenRightBar);
+    };
+
     return (
-        <Routes>
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/portfolio" element={<PortfolioPage />} />
-            <Route path="/education" element={<EducationPage />} />
-            <Route path="/about-me" element={<AboutMePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route
-                path="/"
-                element={<Navigate to="/home" replace />}
-            />
-        </Routes>
+        <>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <AppBarMobileView
+                    drawerSideBarWidth={drawerSideBarWidth}
+                    handleDrawerToggleSideBar={handleDrawerToggleSideBar}
+                    handleDrawerToggleRightBar={handleDrawerToggleRightBar}
+                />
+                <DrawerMobileView
+                    drawerSideBarWidth={drawerSideBarWidth}
+                    mobileOpenSideBar={mobileOpenSideBar}
+                    handleDrawerToggleSideBar={handleDrawerToggleSideBar}
+                    drawerRightBarWidth={drawerRightBarWidth}
+                    mobileOpenRightBar={mobileOpenRightBar}
+                    handleDrawerToggleRightBar={handleDrawerToggleRightBar}
+                />
+            </Box>
+            <Routes>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/portfolio" element={<PortfolioPage />} />
+                <Route path="/education" element={<EducationPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/about-me" element={<AboutMePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route
+                    path="/"
+                    element={<Navigate to="/home" replace />}
+                />
+            </Routes>
+        </>
     );
 };
