c6a89d34f88530c347388f5370a8a4f87ec0dc04
shyBBy - laptop <dawid.a.olczak@gmail.com>
2022-12-02 21:00:01 +0100

    work in progress, refactor HomePage and Hero secti

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,18 +1,25 @@
 import {Box, Container} from '@mui/material'
 import React from 'react'
+import Stack from '@mui/material/Stack';
 import {Hero} from "./Hero/Hero";
 import {MainContent} from "./MainContent/MainContent";
+import './styles.css'
 
-
-interface Props {
-    children: React.ReactNode;
-}
 
 export const Feed = () => {
 
     return (
         <>
-            <MainContent/>
+            <Stack spacing={2}>
+                <Box className='bgimage' sx={{width: '100vw', height: '50vh'}}>
+                    <Box className='mask' sx={{width: '100vw', height: '53vh'}}>
+                        <Hero/>
+                    </Box>
+                </Box>
+                <Box>
+                    <MainContent/>
+                </Box>
+            </Stack>
         </>
     )
 }
--- src/components/Feed/Hero/Hero.tsx
+++ src/components/Feed/Hero/Hero.tsx
@@ -10,31 +10,17 @@ export const Hero = () => {
 
     return(
         <>
-            <Box position={"absolute"}
-                 sx=
-                     {{
-                     top: {xs: '18%', lg: '5%', md: '5%', sm: '5%'},
-                     left: {xs: '3%', lg: '17%', md: '24%', sm: '2%'}
-                    }}
-            >
-                <Typography variant={'h1'}>Cześć, jestem Dawid!</Typography>
-                <Box width={'41%'}>
-                    <Typography variant={'body1'}>Uczący się Full-Stack Developer, jeśli chcesz dowiedzieć się czegoś więcej o mojej historii w programowaniu oraz życiu nie związanym z branża to kliknij w poniższy przycisk lub skorzystaj z bocznego menu :)</Typography>
-                    <Link to={'/portfolio'} style={{textDecoration: 'none'}}><Button variant="outlined">Więcej o mnie...</Button></Link>
-                </Box>
-                <Box
-                    width={'41%'}
-                    position={'absolute'}
-                     sx=
-                    {{
-                        top: {xs: '11%', lg: '65%', md: '70%', sm: '2%'},
-                        left: {xs: '40%', lg: '17%', md: '2%', sm: '2%'}
-                    }}
-                >
-                    <img className={'artImage'} src={art}/>
-                </Box>
-
-
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 2, sm: 2, md: 12 }}>
+                    <Grid item xs={2} sm={2} md={5} >
+                        <Typography variant={'h1'}>Cześć, jestem Dawid!</Typography>
+                        <Typography variant={'body1'}>Uczący się Full-Stack Developer, jeśli chcesz dowiedzieć się czegoś więcej o mojej historii w programowaniu oraz życiu nie związanym z branża to kliknij w poniższy przycisk lub skorzystaj z bocznego menu :)</Typography>
+                        <Link to={'/portfolio'} style={{textDecoration: 'none'}}><Button variant="outlined">Więcej o mnie...</Button></Link>
+                    </Grid>
+                    <Grid item xs={2} sm={2} md={7}>
+                        <img className={'artImage'} src={art}/>
+                    </Grid>
+                </Grid>
             </Box>
         </>
     )
