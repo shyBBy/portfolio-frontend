ba21da85e20f526f350f5758a52ad7d51c5f73b6
dawid.a.olczak@gmail.com <dawid.a.olczak@gmail.com>
2022-12-06 17:03:53 +0100

    test

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -9,19 +9,18 @@ import './styles.css'
 export const Feed = () => {
 
     return (
-        <>
-            <Box sx={{ flexGrow: 1 }}>
-                <Stack spacing={2}>
-                    <Box className='bgimage' sx={{width: '100vw', height: '50vh'}}>
-                        <Box className='mask' sx={{width: '100vw', height: '53vh'}}>
-                            <Hero/>
-                        </Box>
-                    </Box>
-                    <Box>
-                        <MainContent/>
-                    </Box>
-                </Stack>
-            </Box>
+        <>  
+        <Box 
+        sx={{ flexGrow: 1, marginTop: {xs: '5px', sm: 0, md: 0, lg: 0, xl: 0} }} >
+            <Grid container spacing={{ xs: 2, md: 3, xl: 2 }} columns={{ xs: 1, sm: 8, md: 12, lg: 12, xl: 25 }}>
+                <Grid item xs={1} sm={2} md={2} lg={2} xl={1}>
+                    <Hero/>
+                </Grid>
+                <Grid item xs={1} sm={8} md={9} lg={9} xl={20}>
+                    <MainContent/>
+                </Grid>
+            </Grid>
+        </Box>
         </>
     )
 }
--- src/components/Feed/Hero/Hero.tsx
+++ src/components/Feed/Hero/Hero.tsx
@@ -10,18 +10,21 @@ export const Hero = () => {
 
     return(
         <>
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 2, sm: 2, md: 12 }}>
-                    <Grid item xs={2} sm={2} md={5} >
-                        <Typography variant={'h1'}>Cześć, jestem Dawid!</Typography>
-                        <Typography variant={'body1'}>Uczący się Full-Stack Developer, jeśli chcesz dowiedzieć się czegoś więcej o mojej historii w programowaniu oraz życiu nie związanym z branża to kliknij w poniższy przycisk lub skorzystaj z bocznego menu :)</Typography>
-                        <Link to={'/portfolio'} style={{textDecoration: 'none'}}><Button variant="outlined">Więcej o mnie...</Button></Link>
-                    </Grid>
-                    <Grid item xs={2} sm={2} md={7} sx={{display: {xs: 'none', sm: 'none', md: 'block', lg: 'block', xl: 'block'}}}>
-                        <img className={'artImage'} src={art}/>
-                    </Grid>
-                </Grid>
-            </Box>
+          <Box className='bgimage' sx={{width: '100vw', height: '50vh'}}>
+                        <Box className='mask' sx={{width: '100vw', height: '53vh'}}>
+                             <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 2, sm: 2, md: 12 }}>
+                                                <Grid item xs={2} sm={2} md={5} >
+                                                    <Typography variant={'h1'}>Cześć, jestem Dawid!</Typography>
+                                                    <Typography variant={'body1'}>Uczący się Full-Stack Developer, jeśli chcesz dowiedzieć się czegoś więcej o mojej historii w programowaniu oraz życiu nie związanym z branża to kliknij w poniższy przycisk lub skorzystaj z bocznego menu :)</Typography>
+                                                    <Link to={'/portfolio'} style={{textDecoration: 'none'}}><Button variant="outlined">Więcej o mnie...</Button></Link>
+                                                </Grid>
+                                                <Grid item xs={2} sm={2} md={7} sx={{display: {xs: 'none', sm: 'none', md: 'block', lg: 'block', xl: 'block'}}}>
+                                                    <img className={'artImage'} src={art}/>
+                                                </Grid>
+                                            </Grid>
+                        </Box>
+                    </Box>
+           
         </>
     )
 }
