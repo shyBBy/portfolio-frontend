bc076696181f5c7a0dbb1c314069c12974769f4e
dawid.a.olczak@gmail.com <dawid.a.olczak@gmail.com>
2022-12-06 18:20:47 +0100

    test bg

--- src/components/Feed/Hero/Hero.tsx
+++ src/components/Feed/Hero/Hero.tsx
@@ -4,13 +4,15 @@ import art from '../../../assets/img/undraw_hello_re_3evm.svg'
 import './Hero.css'
 import {Link} from "react-router-dom";
 
+import bg from '../../../assets/bg4.jpg'
+
 export const Hero = () => {
 
     const theme = useTheme();
 
     return(
         <>
-            <Box>
+            <Box sx={{backgroundImage: `url(${bg})`}}>
               <Grid container spacing={{ xs: 2, md: 3, xl: 2 }} columns={{ xs: 4, sm: 8, md: 12, lg: 12, xl: 25 }}>
                 <Grid item xs={2} sm={2} md={2} lg={2} xl={1}>
                   <p>jestem dawid</p>
