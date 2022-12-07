f27a8edc3fd64acf71d8b6b72ccacd1b80d42ee9
dawid.a.olczak@gmail.com <dawid.a.olczak@gmail.com>
2022-12-07 11:46:19 +0100

    .

--- src/components/Feed/Hero/Hero.tsx
+++ src/components/Feed/Hero/Hero.tsx
@@ -18,18 +18,27 @@ export const Hero = () => {
               backgroundPosition: 'center',
               backgroundRepeat: 'no-repeat',
               backgroundSize: 'cover',
+              padding: 0,
+              margin: 0,
              
             }}>
               
                 <Grid container spacing={{ xs: 2, md: 3, xl: 2 }} columns={{ xs: 4, sm: 8, md: 12, lg: 12, xl: 25 }}>
                 <Grid item xs={2} sm={2} md={2} lg={2} xl={1}>
-                  <p>jestem dawid</p>
+                  <Typography variant={'h1'}>Cześć, jestem Dawid!</Typography>
                 </Grid>
                 <Grid item xs={2} sm={2} md={2} lg={2} xl={1}>
-                  obrazek
+                  <img 
+                  src={art}
+                  sizes="(maxWidth: 300px) 100px, (maxWidth: 768px) 768px, 1280px" />
                 </Grid>
                 <Grid item xs={2} sm={2} md={2} lg={2} xl={1}>
-                  tu krótki opis
+                   <Typography variant={'body1'}>Uczący się Full-Stack Developer, jeśli chcesz dowiedzieć się czegoś więcej o mojej historii w programowaniu oraz życiu nie związanym z branża to kliknij w poniższy przycisk lub skorzystaj z bocznego menu :)
+                        </Typography>
+                        
+                        <Box sx={{marginTop: '5px'}}>
+                          <Link to={'/portfolio'} style={{textDecoration: 'none'}}><Button variant="outlined">Więcej o mnie...</Button></Link>
+                        </Box>
                 </Grid>
               </Grid>
              
